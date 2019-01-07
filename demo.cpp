0aa6b38885aabe399a0788096dee01e33e85e1e0
Sheng-Hao Ma <aaaddress1@chroot.org>
2019-01-07 23:13:59 +0800

    new demo

---
+++ demo.cpp
@@ -0,0 +1,14 @@
+/* Beep & Alert.
+ * by aaaddress1@chroot.org
+ */
+#include <shellDev>
+
+void shellFunc shellEntry(void) {
+    PVOID addr;
+
+	fetchAPI(msgbox, MessageBoxA);
+	fetchAPI(bp, Beep);
+
+	bp(100, 100);
+	msgbox(0, "hello", "word", 0);
+}
